054c04922e4e3f0af2e62cde47601b112d6741cc
fatiperea <fatimaperea3pppp0@gmail.com>
2024-03-10 12:53:55 -0300

    config archivo estatico en la ruta principal (index.html)

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
-import express  from "express";
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+import morgan from "morgan";
+import path from "path";
+import { fileURLToPath } from "url";
+const app = express();
 
-console.log("hola mundo");
-console.log("hola mundo v2");
+app.set("puerto", process.env.PORT || 4000);
+app.listen(app.get("puerto"), () => {
+  console.log("puerto", app.get("puerto"));
+});
 
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+//console.log(__filename);
+//console.log(path.join(__dirname, "/public"));
+
+app.use(express.static(path.join(__dirname, "/public")));
+
+app.get("/nuevo", (req, res) => {
+  console.log("solicitud get");
+  res.send("ok");
+});
