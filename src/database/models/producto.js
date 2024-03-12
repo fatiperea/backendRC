404728a61ce9f6765e57de5a2fc216a401691e17
fatiperea <fatimaperea3pppp0@gmail.com>
2024-03-12 17:37:43 -0300

    schema

---
+++ src/database/models/producto.js
@@ -0,0 +1,46 @@
+import mongoose, { Schema } from "mongoose";
+
+const produSchema = new Schema({
+  nombreProducto: {
+    type: String,
+    required: true,
+    minLength: 2,
+    maxLength: 50,
+    unique: true,
+  },
+  precio: {
+    type: Number,
+    required: true,
+    min: 50,
+    max: 10000,
+  },
+  imagen: {
+    type: String,
+    required,
+    validate: (valor) => {
+      return /(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|jpeg|gif|png)/.test(valor);
+    },
+    message: (dato) => `${dato.value} no es una URL de imagen valida`,
+  },
+  categoria: {
+    type: String,
+    required: true,
+    enum: ["Infusiones", "Batidos", "Dulce", "Salado"],
+  },
+  descripcion_breve: {
+    type: String,
+    required: true,
+    minLength: 5,
+    maxLength: 50,
+  },
+  descripcion_amplia: {
+    type: String,
+    required: true,
+    minLength: 3,
+    maxLength: 150,
+  },
+});
+
+const Producto = mongoose.model("producto", produSchema);
+
+export default Producto;
